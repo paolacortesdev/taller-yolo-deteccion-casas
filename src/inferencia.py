d477c2f09281e7a7a802e596e6b9932f046333bc
Paola Cortes <pacm6734@gmail.com>
2026-05-14 22:26:44 -0500

    Agregar resultados de inferencia y actualizar proyecto

--- src/inferencia.py
+++ src/inferencia.py
@@ -4,9 +4,9 @@ from ultralytics import YOLO
 model = YOLO("models/house-yolo.pt")
 
 # hacer predicción
-results = model("test.jpg", show=True, conf=0.025)
+results = model("results/test3.jpg", show=True, conf=0.025)
 
 # guardar resultados
-results[0].save(filename="resultado5.jpg")
+results[0].save(filename="results/resultado3.jpg")
 
 print("Predicción completada")
